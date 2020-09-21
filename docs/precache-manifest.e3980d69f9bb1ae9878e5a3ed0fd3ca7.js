e25d09aae76baac5d6576fae3638bdd9354106d0
Agustin Cuesta <agustin.n.cuesta@gmail.com>
2020-09-21 17:15:10 -0300

    Add docs

---
+++ docs/precache-manifest.e3980d69f9bb1ae9878e5a3ed0fd3ca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee1177fdc87f5b0f92cb354aad43747",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a077dc8f01db23026eca",
+    "url": "/static/css/main.beafe63f.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a077dc8f01db23026eca",
+    "url": "/static/js/main.baea393d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
